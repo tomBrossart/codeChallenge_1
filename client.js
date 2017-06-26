8197fb8ba6406e1f1bf933a5e479ad08423dd896
Tom Brossart <tom@blueraven.digital>
2017-06-26 14:36:46 -0500

    last try to fix counter

--- client.js
+++ client.js
@@ -4,41 +4,33 @@ var buttonCounter = 0;
 
 $(document).ready(function() {
   addClickHandlers();
-  appendDOM();
-
-
-
-
-
 });
 
 function addClickHandlers() {
   $('body').on('click', '#generate', function() {
     buttonCounter += 1;
     $('body').append('<div class="red"></div>');
-    $('div:first-of-type').html('<p></p>');
-    $('div p').text(buttonCounter);
-    $('div:first-of-type').html('<button type="button" id="swap">Swap</button>' +
+    $('div').html('<button type="button" id="swap">Swap</button>' +
                             '<button type="button" id="delete">Delete</button>');
+    $('div').last().append('<p>' + buttonCounter + '</p>');
+    // can't figure out why this is clearing the previous p elements....
     console.log(buttonCounter);
     });
   $('body').on('click', '#swap', function() {
-    $(this).parent().removeClass('red');
+    $(this).parent().toggleClass('red');
   });
   $('body').on('click', '#delete', function() {
     $(this).parent().remove();
+    console.log(buttonCounter);
   });
 }
 
-function appendDOM() {
-
-}
 
 
 
 
 
-
-// Buttons for later
-$('.container').append('<button type="button" id="swap">Swap</button>' +
-                        '<button type="button" id="delete">Delete</button>');
+//
+// // Buttons for later
+// $('.container').append('<button type="button" id="swap">Swap</button>' +
+//                         '<button type="button" id="delete">Delete</button>');
