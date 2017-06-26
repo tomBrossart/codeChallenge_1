4e878d72ba29eb4324cdca7a1dd4da47b39a9b85
Tom Brossart <tom@blueraven.digital>
2017-06-26 14:01:21 -0500

    added click events for swap and delete buttons

--- client.js
+++ client.js
@@ -15,15 +15,19 @@ $(document).ready(function() {
 function addClickHandlers() {
   $('body').on('click', '#generate', function() {
     buttonCounter += 1;
-    $('body').append('<div class="generated"></div>');
+    $('body').append('<div class="red"></div>');
     $('div:first-of-type').html('<p></p>');
     $('div p').text(buttonCounter);
     $('div:first-of-type').html('<button type="button" id="swap">Swap</button>' +
                             '<button type="button" id="delete">Delete</button>');
-
-
     console.log(buttonCounter);
     });
+  $('body').on('click', '#swap', function() {
+    $(this).parent().removeClass('red');
+  });
+  $('body').on('click', '#delete', function() {
+    $(this).parent().remove();
+  });
 }
 
 function appendDOM() {
